719e928c2871358455702efaa81f3d4c6a636ee9
Max Klassen <maximiliankk@gmail.com>
2018-06-30 21:30:38 -0700

    coin loading

--- src/cute_jam/cute.cpp
+++ src/cute_jam/cute.cpp
@@ -38,6 +38,8 @@
 #include <cute_jam/coin.h>
 #include <cute_jam/dog.h>
 
+#include <cute_jam/load_objects_from_map_cpp.h>
+
 // This must come last (or at least after all entity types, so the global vtable can be constructed).
 #include <cute_jam/entity_cpp.h>
 
@@ -72,9 +74,6 @@ EXPORT int single_time_initialization(launcher_t* launcher)
 	// Setup the player.
 	env->player = create_player();
 	create_dog();
-
-	coin_t* coin = create_coin();
-	set_coin_position(coin, -100, 0);
 
 	srand(time(0));
 
